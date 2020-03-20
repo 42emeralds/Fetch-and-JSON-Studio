08b9e48ae154bccfd13000dcc01a80fd5a08047f
Emerald <emerald_mg@yahoo.com>
2020-03-19 21:18:52 -0500

    Done without bonus

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// TODO: add code here
+
+window.addEventListener("load", function () { 
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+      response.json().then( function(json) {
+        const astronauts = document.getElementById("container");
+        for(let index = 0; index < json.length; index++) { 
+        astronauts.innerHTML += `
+          <div class="astronaut">
+            <div class = "bio">
+                <h3>${json[index].firstName} ${json[index].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${json[index].hoursInSpace}</li>
+                        <li id="activeStatus">Active: ${json[index].active}</li>
+                        <li>Skills: ${json[index].skills}</li>
+                    </ul>
+            </div>
+            <img class="avatar" src=${json[index].picture}></img>
+          </div>
+         `;
+        };
+      });
+  });
+});
